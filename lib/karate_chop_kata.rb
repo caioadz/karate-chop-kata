90482e927e4dc97e73d963bb3ed593e13e7de466
Caio Zem <caioadz@hotmail.com>
2019-05-23 08:17:41 -0300

    Initial commit

---
+++ lib/karate_chop_kata.rb
@@ -0,0 +1,6 @@
+require "karate_chop_kata/version"
+
+module KarateChopKata
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/karate_chop_kata/version.rb
@@ -0,0 +1,3 @@
+module KarateChopKata
+  VERSION = "0.1.0"
+end
